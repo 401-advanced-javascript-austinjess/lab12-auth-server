307417b0b054b223db4289b2bd8e7d1dd9a26d83
Austin Jess <ajess33@gmail.com>
2019-06-04 15:21:42 -0500

    google oauth is setup

--- src/auth/router.js
+++ src/auth/router.js
@@ -9,27 +9,29 @@ const oauth = require('./oauth/google.js');
 
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
-  user.save()
-    .then( (user) => {
+  user
+    .save()
+    .then((user) => {
       req.token = user.generateToken();
       req.user = user;
       res.set('token', req.token);
       res.cookie('auth', req.token);
       res.send(req.token);
-    }).catch(next);
+    })
+    .catch(next);
+});
+
+authRouter.get('/oauth', (req, res, next) => {
+  oauth(req)
+    .then((token) => {
+      res.status(200).send(token);
+    })
+    .catch(next);
 });
 
 authRouter.post('/signin', auth, (req, res, next) => {
   res.cookie('auth', req.token);
   res.send(req.token);
-});
-
-authRouter.get('/oauth', (req,res,next) => {
-  oauth(req)
-    .then( token => {
-      res.status(200).send(token);
-    })
-    .catch(next);
 });
 
 module.exports = authRouter;
